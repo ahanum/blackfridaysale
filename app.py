51657fa3f1a55d729cb6a27bbb5a9b1bf065d3bb
ahanum <100678287+ahanum@users.noreply.github.com>
2022-03-09 18:27:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -32,6 +32,13 @@ def main():
 if __name__ == '__main__':
 	main()
 	
+	
+# Use local CSS
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+local_css("style/style.css")
+
 animation_symbol = "❄"
 
 st.markdown(
